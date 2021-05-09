fa46f07e5a869e69a56e2645a4b4e46e7b37eba2
Nagy László Endre <nlaciendre@gmail.com>
2021-05-09 21:20:01 +0200

    Implemented isValidGameState method, updated isGoal and Test methods.

--- src/test/java/GameStateTest.java
+++ src/test/java/GameStateTest.java
@@ -8,26 +8,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class GameStateTest {
 
+
+    GameState gameStateBase=GameState.createNewGame();
+    GameState gameState1=GameState.loadGame(0,1,new King(3,1),new King(3,7),GameState.createEmptyTiles());
+    GameState gameState2=GameState.loadGame(0,1,new King(0,0),new King(4,4),GameState.createEmptyTiles());
+
+
     @Test
     void testOperator(){
         GameState gameState=GameState.createNewGame();
         gameState.applyOperator(new Position(3,1));
-        assertEquals( gameState,(GameState.loadGame(0,1,new King(3,1),new King(3,7),GameState.createEmptyTiles())));
+        assertEquals((gameState1), gameState);
         gameState.applyOperator(new Position(4,4));
-        SquareStatus[][] tiles =GameState.createEmptyTiles();
-        tiles[4][4]=SquareStatus.REMOVED;
-        assertEquals(gameState,(GameState.loadGame(1,0,new King(3,1),new King(3,7),tiles)));
+        gameState2.setTile(0,1,SquareStatus.REMOVED);
+        gameState2.setTile(1,1,SquareStatus.REMOVED);
+        assertEquals(1,gameState2.applyOperator(new Position(1,0)));
+
 
     }
     @Test
     void testIsAppliable(){
         GameState gameState=GameState.createNewGame();
-        assertFalse(gameState.isAppliable(new Position(3,3)));
-        gameState.applyOperator(new Position(3,1));
-        assertFalse(gameState.isAppliable(new Position(3,1)));
-        assertTrue(gameState.isAppliable(new Position(2,7)));
-
-
+        assertFalse(gameStateBase.isAppliable(new Position(3,3)));
+        assertTrue(gameState.isAppliable(new Position(3,1)));
+        assertFalse(gameState.isAppliable(new Position(2,7)));
+        gameState.setMoveIndex(1);
+        assertFalse(gameState.isAppliable(new Position(3,7)));
     }
 
     @Test
@@ -36,9 +42,57 @@ public class GameStateTest {
         tiles[0][1]=SquareStatus.REMOVED;
         tiles[1][1]=SquareStatus.REMOVED;
         tiles[1][0]=SquareStatus.REMOVED;
-        GameState gameState=GameState.loadGame(0,0,new King(0,0),new King(4,4),tiles);
-        assertEquals(gameState.isGoal(), 1);
+        GameState gameState=GameState.loadGame(1,1,new King(0,0),new King(4,4),tiles);
+        assertEquals( 1,gameState.isGoal());
+        gameState.setWhiteKing(new King(3,3));
+        assertEquals( -1,gameState.isGoal());
     }
+
+    @Test
+    void testIsEmpty(){
+        GameState gameState=GameState.createNewGame();
+        gameState.applyOperator(new Position(3,1));
+    }
+
+    @Test
+    void testIsNeighbour(){
+       assertTrue(GameState.isNeighbour(new Position(3,4),new Position(4,5)));
+        assertTrue(GameState.isNeighbour(new Position(11,11),new Position(11,12)));
+        assertFalse(GameState.isNeighbour(new Position(11,11),new Position(1,12)));
+    }
+
+    @Test
+    void testIsInPlayField(){
+        assertTrue(gameStateBase.isInPlayField(new Position(5,6)));
+        assertFalse(gameStateBase.isInPlayField(new Position(8,4)));
+    }
+
+    @Test
+    void testIsKingMoveAppliable(){
+        assertTrue(gameStateBase.isKingMoveAppliable(new Position(5,6),new Position(5,7)));
+        assertFalse(gameStateBase.isKingMoveAppliable(new Position(7,3),new Position(8,4)));
+    }
+
+    @Test
+    void testIsValidGameState(){
+        assertTrue(gameStateBase.isValidGameState());
+        assertTrue(gameState2.isValidGameState());
+        gameStateBase.setWhiteKing(new King(8,4));
+        assertFalse(gameStateBase.isValidGameState());
+    }
+
+    @Test
+
+    void testLoadGame()  {
+
+        assertEquals(GameState.createNewGame(),
+                GameState.loadGame(0,0,new King(2,0),new King(3,7),GameState.createEmptyTiles()));
+
+        assertThrows(IllegalArgumentException.class,()->GameState.loadGame(0,0,new King(2,0),new King(7,7),GameState.createEmptyTiles()));
+
+    }
+
+
 
 
 
